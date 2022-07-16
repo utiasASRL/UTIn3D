91b59246fe5e0b9b2bb5bd8f595962f5bec003ff
HuguesTHOMAS <huguesthomas218@gmail.com>
2022-07-16 09:35:46 -0400

    .

--- Scripts/download.py
+++ Scripts/download.py
@@ -53,80 +53,15 @@ if __name__ == '__main__':
     folders = ['annotated_frames', 'annotation', 'calibration', 'collisions', 'runs', 'slam_offline']
     for path in [UTIn3D_A_path, UTIn3D_H_path]:
         for folder in folders:
+
+            # Unpack
             folder_path = os.path.join(path, folder)
             zipfile = folder_path + '.zip'
             shutil.unpack_archive(zipfile, folder_path)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    # Parameters
-    roots = ['../Data/UTI3D_A', '../Data/UTI3D_H']
-    folders = ['annotated_frames',
-               'annotation',
-               'calibration',
-               'collisions',
-               'runs',
-               'slam_offline']
-
-    gdrive_parents = ['1fCffwd_Z9v6886LzO9RmkAMGUdaqAX7t', '1-XRsO3V5yh6iSZgznRORKP7RoKbWSi2a']
-
-
-    for root, gdrive_parent in zip(roots, gdrive_parents):
-
-        print('\n')
-        print('-------------------------------------------------------------------------')
-        print('\nPreparing' + root)
-        print('*********' + '*' * len(root))
-
-
-        for folder in folders:
-
-            folder_path = join(root, folder)
-            print('\n    ' + folder_path)
-
-            # First zip folder
-            print('        > zipping')
-            shutil.make_archive(folder_path, 'zip', folder_path)
-
-            # Upload the zip file on google drive
-            print('        > uploading')
-            zip_file = folder_path + '.zip'
-            os.system('gdrive upload -p {:s} {:s}'.format(gdrive_parent, zip_file))
-
             # Now remove the file to save space
-            print('        > removing')
             if exists(zip_file):
                 remove(zip_file)
-    
-
-
-
-
-
-
-
-
-
-
-
 
 
 
